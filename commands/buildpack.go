32296fdb743ed52a84d98e5c536206bcac0e401f
James Williams <jwilliams@pivotal.io>
2018-02-13 09:07:26 -0500

    cli cleanup and module work. each category will have its own go file

---
+++ commands/buildpack.go
@@ -0,0 +1,110 @@
+package commands
+
+import (
+	"gopkg.in/urfave/cli.v1"
+	"os"
+	"paap/smokehttp"
+	"paap/cmd"
+)
+
+var BuildpackCommands = []cli.Command{
+	{
+		Name:  "login",
+		Usage: "login to cloud foundry using env vars for api, username, password",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			cmd.RunCommands(`cf login -a $CF_API -u $CF_USER -p $CF_PASS --skip-ssl-validation`)
+			return nil
+		},
+	},
+	{
+		Name:  "create-lob",
+		Usage: "create pre-defined org with 2 spaces and small quotas",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			commands := `cf org demo
+					cf create-quota small-org -m 10G -a 10 -r 100
+					cf create-org demo -q small-org
+					cf create-space development -o demo
+					cf create-space production -o demo
+					cf org demo
+		            cf quota small-org`
+
+			cmd.RunCommands(commands)
+			return nil
+		},
+	},
+	{
+		Name:  "deploy-app",
+		Usage: "deploy application to the 'development' or 'production' space based on $CF_SPACE env variable",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			var commands string
+			if (os.Getenv("CF_SPACE") == "development") {
+				commands = `cf target -o $CF_ORG -s $CF_SPACE
+								cf push -f ./bin/springboot-app/manifest-development.yml`
+			}
+			if (os.Getenv("CF_SPACE") == "production") {
+				commands = `cf target -o $CF_ORG -s $CF_SPACE
+								cf push -f ./bin/springboot-app/manifest-production.yml`
+			}
+
+			cmd.RunCommands(commands)
+			return nil
+		},
+	},
+	{
+		Name:  "run-smoketest",
+		Usage: "HTTP GET smoke test that runs until error and loops every 3 seconds",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			smokehttp.SmokeHttp("http://springboot-app-development.local.pcfdev.io", 3)
+			return nil
+		},
+	},
+	{
+		Name:  "install-plugins",
+		Usage: "Install 'buildpack-usage', 'do-all' and 'top' cf plugins",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			commands := `cf plugins
+		             cf install-plugin -f -r CF-Community buildpack-usage
+		             cf install-plugin -f -r CF-Community do-all
+					 cf install-plugin -f -r CF-Community top
+		             cf plugins`
+
+			cmd.RunCommands(commands)
+			return nil
+		},
+	},
+	{
+		Name:  "upgrade-middleware",
+		Usage: "upgrade to java v4.8 buildpack and restage all apps in $CF_SPACE",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			cmd.PivnetGet(
+				"https://network.pivotal.io/api/v2/products/buildpacks/releases/31948/product_files/62976/download",
+				"./bin/buildpacks/java-buildpack-offline-v4.8.zip")
+
+			commands := `cf create-buildpack --enable java_buildpack-v48 ./bin/buildpacks/java-buildpack-offline-v4.8.zip 1
+						             cf buildpacks
+						             cf target -o $CF_ORG -s $CF_SPACE
+									 cf do-all restage {}
+						             cf buildpack-usage -b java_buildpack
+						             cf buildpack-usage -b java_buildpack-v48`
+
+			cmd.RunCommands(commands)
+			return nil
+		},
+	},
+	{
+		Name:  "teardown",
+		Usage: "delete pre-defined org including spaces and applications",
+		Category: "Buildpack Demo",
+		Action: func(c *cli.Context) error {
+			cmd.RunCommands(`cf delete-org -f $CF_ORG
+									   cf delete-buildpack -f java_buildpack-v48`)
+			return nil
+		},
+	},
+}
--- main.go
+++ main.go
@@ -2,99 +2,18 @@ package main
 
 import (
 	"os"
-    "fmt"
-    "paap/cmd"
-    "paap/smokehttp"
+    "paap/commands"
+	"gopkg.in/urfave/cli.v1"
 )
 
 func main() {
+	app := cli.NewApp()
+	app.Name = "paap"
+	app.Version = "0.0.1"
+	app.Description = "Demo CLI for cloud ops"
 
-	switch os.Args[1] {
+	app.Commands = append(app.Commands, commands.BuildpackCommands...)
 
-	case "login" :
-		cmd.RunCommands(`cf login -a $CF_API -u $CF_USER -p $CF_PASS --skip-ssl-validation`)
 
-	case "create-lob":
-		commands := `cf org demo
-					cf create-quota small-org -m 10G -a 10 -r 100
-					cf create-org demo -q small-org
-					cf create-space development -o demo
-					cf create-space production -o demo
-					cf org demo
-		            cf quota small-org`
-
-		cmd.RunCommands(commands)
-
-	case "teardown":
-		cmd.RunCommands(`cf delete-org -f $CF_ORG
-								   cf delete-buildpack -f java_buildpack-v48`)
-
-		if (len(os.Args) > 2){
-			cmd.RunCommands(`cf uninstall-plugin buildpack-usage
-			                           cf uninstall-plugin do-all
-			                           cf uninstall-plugin top`)
-		}
-
-	case "deploy-app":
-
-		var commands string
-
-		if (os.Getenv("CF_SPACE") == "development"){
-			commands = `cf target -o $CF_ORG -s $CF_SPACE
-					    cf push -f ./bin/springboot-app/manifest-development.yml`
-		}
-
-		if (os.Getenv("CF_SPACE") == "production"){
-			commands = `cf target -o $CF_ORG -s $CF_SPACE
-					    cf push -f ./bin/springboot-app/manifest-production.yml`
-		}
-
-		cmd.RunCommands(commands)
-
-	case "run-smoketest" :
-		smokehttp.SmokeHttp("http://springboot-app-development.local.pcfdev.io", 3)
-
-	case "install-plugins":
-		commands := `cf plugins
-		             cf install-plugin -f -r CF-Community buildpack-usage
-		             cf install-plugin -f -r CF-Community do-all
-					 cf install-plugin -f -r CF-Community top
-		             cf plugins`
-
-		cmd.RunCommands(commands)
-
-	case "upgrade-middleware":
-		cmd.PivnetGet(
-			"https://network.pivotal.io/api/v2/products/buildpacks/releases/31948/product_files/62976/download",
-			"./bin/buildpacks/java-buildpack-offline-v4.8.zip")
-
-		commands := `cf create-buildpack --enable java_buildpack-v48 ./bin/buildpacks/java-buildpack-offline-v4.8.zip 1
-		             cf buildpacks
-		             cf target -o $CF_ORG -s $CF_SPACE
-					 cf do-all restage {}
-		             cf buildpack-usage -b java_buildpack
-		             cf buildpack-usage -b java_buildpack-v48`
-
-		cmd.RunCommands(commands)
-
-	case "help":
-		fmt.Println("\nSETUP: You must set environment variables. ")
-		fmt.Println("Refer to env_development for guidance")
-		fmt.Println("     CF_API")
-		fmt.Println("     CF_USER")
-		fmt.Println("     CF_PASS")
-		fmt.Println("     CF_ORG")
-		fmt.Println("     CF_SPACE")
-		fmt.Println("	 CF_NETWORK_TOKEN")
-		fmt.Println("\nRUN: You can run the following commands:")
-		fmt.Println("     login")
-		fmt.Println("     install-plugins")
-		fmt.Println("     create-lob")
-		fmt.Println("     deploy-app")
-		fmt.Println("     run-smoketest")
-		fmt.Println("     upgrade-middleware\n")
-		fmt.Println("     teardown")
-		fmt.Println("     teardown all")
-	}
-
+	app.Run(os.Args)
 }
